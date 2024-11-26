6a0c28dfa2a4ad1825e73be003b1b475ce93e673
hhoang777 <hhoang1072003@gmail.com>
2024-11-26 23:07:12 +0700

    server_3

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Save and see your changes instantly33333.</li>
+          <li>Save and see your changes instantly3333355555555.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World!!!!!!!!!222222");
+  res.send("Hello World!!!!!!!!!222222444444");
 });
 app.listen(3009, function () {
   console.log("Example app listening on port 3000!");
