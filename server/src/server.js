a222618cb3a6dd898e4b496076d73825180472a6
hhoang777 <hhoang1072003@gmail.com>
2024-11-27 16:20:31 +0700

    client_server

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World!!!!!!!!!222222444444");
+  res.send("Hello World");
 });
 app.listen(3009, function () {
   console.log("Example app listening on port 3000!");
